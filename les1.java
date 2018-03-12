bfc4064fc70bca8f6e3e6be4f7db770e6bf52099
Thom Rustemeijer <24703@ma-web.nl>
2018-03-12 14:16:05 +0100

    Initial commit

---
+++ les1.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class les1 {
+
+    public static void main(String[] args) {
+        //ODRACHT 1.2
+        System.out.println("Java is Object Georiënteerd");
+        System.out.println("Java is strongly typed");
+        System.out.println("Java is geen JavaScript");
+        //OPDRACHT 1.2
+        System.out.println("Java is geweldig!");
+        System.out.println("Java is geweldig!");
+        System.out.println("Java is geweldig!");
+        System.out.println("Java is geweldig!");
+        System.out.println("Java is geweldig!");
+        //OPDRACHT 1.3
+        System.out.println("De output op je scherm moet zijn:\n" +
+                "a\ta^2\ta^3\ta^4\n" +
+                "1\t1\t1\t1\n" +
+                "2\t4\t8\t16\n" +
+                "3\t9\t27\t81\n" +
+                "4\t16\t64\t256");
+        //OPDRACHT 1.4
+        System.out.println((7.5 * 6.5 - 4.5 * 3) / (47.5 * 5.5));
+        //OPDRACHT 1.5
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+        //OPDRACHT 1.6
+        System.out.println(4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13));
+        //OPDRACHT 1.7
+        System.out.println(6.5 * 6.5 * 3.14159);
+        System.out.println(2 * 6.5 * 3.14159);
+        //OPDRACHT 1.8
+        System.out.println(5.3 * 8.6);
+        System.out.println(2 * (5.3 + 8.6));
+    }
+}
